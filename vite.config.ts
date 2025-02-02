8db29b73256fa70ba21962ce6831fb78a7e8f319
Christian Sequeira <csequeira153@gmail.com>
2025-02-02 00:53:47 -0300

    feat: update .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
 
-// https://vite.dev/config/
+dotenv.config();
+
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    define: {
+        'process.env.PASSCODE_SECRET_KEY': JSON.stringify(process.env.PASSCODE_SECRET_KEY),
+    },
 })
